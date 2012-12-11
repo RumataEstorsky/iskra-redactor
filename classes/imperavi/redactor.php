e37094a241ea3b426873c7e4fe23104c9bfe8e1a
Rumata Estorsky <rumata@sputnikchess.ru>
2012-12-11 13:25:43 +0400

    etc

--- classes/imperavi/redactor.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class Imperavi_Redactor {
-    public static function init($name) {
-        StaticCss::instance()->add('/js/redactor/css/redactor.css');
-        StaticJs::instance()->add('/js/redactor/redactor.js');
-        StaticJs::instance()->add_inline(View::factory('imperavi_redactor/init', array('name' => $name)));
-    }
-}
